6d966cd6bbf5c3ff168efae3dca290d6f1cd3b3c
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-06 17:14:35 +0100

    Add Header component and App styled with general sass file

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,23 @@
 </script>
 
 <template>
-
+    <div class="container">
+        <nav class="row">
+            <ul class="col display-flex header-gender">
+                <li><a href="#">lorem</a></li>
+                <li><a href="#">lorem</a></li>
+                <li><a href="#">lorem</a></li>
+            </ul>
+            <figure class="col display-flex ">
+                <img src="/img/boolean-logo.pngS" alt="" class="header-logo">
+            </figure>
+            <ul class="col display-flex header-icons">
+                <li><a href="#">&star;</a></li>
+                <li><a href="#">&star;</a></li>
+                <li><a href="#">&star;</a></li>
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <style lang="scss" scoped>
