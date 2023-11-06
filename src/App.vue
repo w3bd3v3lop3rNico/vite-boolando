b4f87d125bded6e1bcfb9f38e9ae5eb956123829
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-06 16:34:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<template>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style lang="scss">
+@use './styles/general.scss'
+
+</style>
